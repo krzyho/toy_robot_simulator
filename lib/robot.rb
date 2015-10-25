cf1362a40929cbd78d2d53c98d6fcf8783abfdc2
Krzysztof Mycek <krzysiek.mycek@gmail.com>
2015-10-25 22:04:23 +1100

    Implemented #place, #left and #right methods

--- lib/robot.rb
+++ lib/robot.rb
@@ -2,15 +2,20 @@ class Robot
   attr_accessor :x, :y, :direction
 
   def place(x, y, direction)
+    self.x = x
+    self.y = y
+    self.direction = direction
   end
 
   def move
   end
 
   def left
+    self.direction = (self.direction > 1) ? self.direction - 1 : 4
   end
 
   def right
+    self.direction = (self.direction < 4) ? self.direction + 1 : 1
   end
 
   def report
