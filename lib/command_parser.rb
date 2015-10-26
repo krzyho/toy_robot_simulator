1c14f88c0689eb78944f62a5a7f8513be1debce6
Krzysztof Mycek <krzysiek.mycek@gmail.com>
2015-10-26 21:58:13 +1100

    Implemented parse_command method

--- lib/command_parser.rb
+++ lib/command_parser.rb
@@ -1,7 +1,18 @@
 class CommandParser
   class << self
     def parse_command(command_string)
-      IgnoreCommand.new
+      case command_string
+      when 'MOVE'
+        return MoveCommand.new
+      when 'LEFT'
+        return LeftCommand.new
+      when 'RIGHT'
+        return RightCommand.new
+      when 'REPORT'
+        return ReportCommand.new
+      end
+      return PlaceCommand.new if command_string =~ /PLACE \d+,\d+,NORTH|EAST|SOUTH|WEST/
+      return IgnoreCommand.new
     end
   end
 end
