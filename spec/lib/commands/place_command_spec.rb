0b1d3c449cbbfa179820eb3f3a550a5b32c12949
Krzysztof Mycek <krzysiek.mycek@gmail.com>
2015-10-27 08:31:51 +1100

    Added negative path spec for PLACE command

--- spec/lib/commands/place_command_spec.rb
+++ spec/lib/commands/place_command_spec.rb
@@ -11,5 +11,13 @@ describe PlaceCommand do
     it 'returns true' do
       expect(subject).to be_truthy
     end
+
+    context "when PLACE parameters are out of range" do
+      let(:command) { PlaceCommand.new('10', '2', 'NORTH') }
+
+      it 'returns false' do
+        expect(subject).to be_falsy
+      end
+    end
   end
 end
