bc38a935ecc7fe46066f66a8e586bb173180a24f
Krzysztof Mycek <krzysiek.mycek@gmail.com>
2015-10-26 16:27:10 +1100

    Refactor Board.move_valid?

--- lib/board.rb
+++ lib/board.rb
@@ -2,18 +2,7 @@ class Board
   UNITS = 5
 
   def move_valid?(x, y, direction)
-    if direction % 2 == 0
-      if direction < 3 
-        x < UNITS - 1
-      else
-        x > 1
-      end
-    else
-      if direction < 3 
-        y < UNITS - 1
-      else
-        y > 1
-      end
-    end
+    axis = direction % 2 == 0 ? 'x' : 'y'
+    direction < 3 ? eval(axis) < UNITS - 1 : eval(axis) > 1
   end
 end
