ab2d7fb386a103a89178f8c9b1fa5669fb07192e
Krzysztof Mycek <krzysiek.mycek@gmail.com>
2015-10-27 12:48:46 +1100

    Added Simulator class which initializes Robot, Board and continously reads STDIN for commands - this is the entry point of the application

--- lib/toy_robot_simulator.rb
+++ lib/toy_robot_simulator.rb
@@ -1,5 +1,27 @@
 require "toy_robot_simulator/version"
+require "robot"
+require "board"
+require "command_parser"
+Dir['./lib/commands/*.rb'].each { |file| require file }
 
 module ToyRobotSimulator
-  # Your code goes here...
+  class Simulator
+
+    def initialize
+      @robot = Robot.new
+      @board = Board.new(5, 5)
+    end
+
+    def start_simulator
+      begin
+        loop do
+          command = CommandParser.parse_command(STDIN.gets)
+          command.execute(@robot) if command.valid?(@robot, @board)
+        end
+      rescue SystemExit, Interrupt => e
+        # rescue from CTRL-C
+        exit
+      end
+    end
+  end
 end
